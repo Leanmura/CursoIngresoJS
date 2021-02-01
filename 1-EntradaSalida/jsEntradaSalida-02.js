98198bea3d442449a6d36c4e53da88ff7f5c65bd
Leanmura <murakoshi.lean@gmail.com>
2021-02-01 10:31:43 -0300

    Ejercicios resueltos

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,11 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	alert("ok");
+function mostrar() {
+	var name; // defino la variable
+	console.log(name);
+	name = prompt("Por favor ingrese su nombre"); // cargo la variable
+	console.log(name);
+	alert(name); // muestro la variable
+	console.log("Hola");
 }
-
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,13 @@
 /*
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
+function mostrar() {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	// nombreIngresado = txtIdNombre.value;  forma 1
+	nombreIngresado = document.getElementById("txtIdNombre").value; // forma 2
 
+	console.log(nombreIngresado);
+	alert(nombreIngresado);
 }
 
-
+/* txtIdNombre es el ID del input del html*/
