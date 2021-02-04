782f5ac47d805903a53109b4f663f31e6ac04429
Leanmura <murakoshi.lean@gmail.com>
2021-02-04 12:50:13 -0300

    resueltos

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,78 @@
-/*1.	Para el departamento de facturación:
+/*
+Leandro Murakoshi
+
+1.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{
-	
+function Sumar() {
+	let precioUno;
+	let precioDos;
+	let precioTres;
+	let suma;
+
+	precioUno = document.getElementById("txtIdPrecioUno").value;
+	precioUno = parseInt(precioUno);
+
+	precioDos = document.getElementById("txtIdPrecioDos").value;
+	precioDos = parseInt(precioDos);
+
+	precioTres = document.getElementById("txtIdPrecioTres").value;
+	precioTres = parseInt(precioTres);
+
+	suma = precioUno + precioDos + precioTres;
+
+	alert("La suma es " + suma);
 }
-function Promedio () 
-{
-	
+function Promedio() {
+	let precioUno;
+	let precioDos;
+	let precioTres;
+	let suma;
+	let promedio;
+
+	precioUno = document.getElementById("txtIdPrecioUno").value;
+	precioUno = parseInt(precioUno);
+
+	precioDos = document.getElementById("txtIdPrecioDos").value;
+	precioDos = parseInt(precioDos);
+
+	precioTres = document.getElementById("txtIdPrecioTres").value;
+	precioTres = parseInt(precioTres);
+
+	suma = precioUno + precioDos + precioTres;
+
+	promedio = suma / 3;
+
+	alert("El promedio es " + promedio);
 }
-function PrecioFinal () 
-{
-	
+function PrecioFinal() {
+	let precioUno;
+	let precioDos;
+	let precioTres;
+	let suma;
+	let precioFinal;
+	let porcentaje;
+	let iva;
+
+	precioUno = document.getElementById("txtIdPrecioUno").value;
+	precioUno = parseInt(precioUno);
+
+	precioDos = document.getElementById("txtIdPrecioDos").value;
+	precioDos = parseInt(precioDos);
+
+	precioTres = document.getElementById("txtIdPrecioTres").value;
+	precioTres = parseInt(precioTres);
+
+	porcentaje = 21;
+	porcentaje = 21 / 100;
+
+	suma = precioUno + precioDos + precioTres;
+
+	iva = suma * porcentaje;
+
+	precioFinal = suma + iva;
+
+	alert("El precio final+IVA es " + precioFinal);
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -4,15 +4,79 @@ B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un ter
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
 
-function Rectangulo () 
-{
+function Rectangulo() {
+	let largo;
+	let ancho;
+	let cantidadDeHilos;
+	let diametro;
+	let totalAlambre;
 
+	cantidadDeHilos = 3;
+
+	largo = document.getElementById("txtIdLargo").value;
+	largo = parseInt(largo);
+
+	ancho = document.getElementById("txtIdAncho").value;
+	ancho = parseInt(ancho);
+
+	diametro = largo + largo + ancho + ancho;
+
+	totalAlambre = diametro * cantidadDeHilos;
+
+	alert("Cantidad de alambre a comprar " + totalAlambre + "m");
 }
-function Circulo () 
-{
-	
+function Circulo() {
+	let radio;
+	let diametro;
+	let perimetro;
+	let totalAlambre;
+	let cantidadDeHilos;
+	let pi;
+
+	pi = 3.14;
+	cantidadDeHilos = 3;
+
+	radio = document.getElementById("txtIdRadio").value;
+	radio = parseInt(radio);
+
+	diametro = radio * 2;
+
+	perimetro = diametro * pi;
+
+	totalAlambre = perimetro * cantidadDeHilos;
+
+	alert("Cantidad de alambre a comprar " + totalAlambre + "m");
 }
-function Materiales () 
-{
-	
+function Materiales() {
+	let largo;
+	let ancho;
+	let area;
+	let cantidadCal;
+	let cantidadCemento;
+	let mensaje;
+
+	largo = document.getElementById("txtIdLargo").value;
+	largo = parseInt(largo);
+
+	ancho = document.getElementById("txtIdAncho").value;
+	ancho = parseInt(ancho);
+
+	area = largo * ancho;
+
+	cantidadCemento = area * 2;
+
+	cantidadCal = area * 3;
+
+	mensaje =
+		"Para hacer un contrapiso de " +
+		ancho +
+		"m x " +
+		largo +
+		"m se necesitan: " +
+		cantidadCal +
+		" bolsas de cal y " +
+		cantidadCemento +
+		" bolsas de cemento";
+
+	alert(mensaje);
 }
