0ffa6598b3ef5511e9b1a88876edfec1e7cd8b08
Leanmura <murakoshi.lean@gmail.com>
2021-02-07 15:13:08 -0300

    resueto

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -8,7 +8,71 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
-function CalcularPrecio () 
-{
- 	
+function CalcularPrecio() {
+	let precioLampara;
+	let cantidadLamparas;
+	let precioTotal;
+	let precioTotalDescuento;
+	let marca;
+	let descuento;
+	let ingresosBrutos;
+
+	precioLampara = 35;
+
+	cantidadLamparas = document.getElementById("txtIdCantidad").value;
+	cantidadLamparas = parseInt(cantidadLamparas);
+
+	marca = document.getElementById("Marca").value;
+
+	precioTotal = cantidadLamparas * precioLampara;
+
+	if (cantidadLamparas > 5) {
+		descuento = (precioTotal * 50) / 100;
+		precioTotalDescuento = precioTotal - descuento;
+	} else {
+		if (cantidadLamparas == 5) {
+			if (marca == "ArgentinaLuz") {
+				descuento = (precioTotal * 40) / 100;
+				precioTotalDescuento = precioTotal - descuento;
+			} else {
+				descuento = (precioTotal * 30) / 100;
+				precioTotalDescuento = precioTotal - descuento;
+			}
+		} else {
+			if (cantidadLamparas == 4) {
+				if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") {
+					descuento = (precioTotal * 25) / 100;
+					precioTotalDescuento = precioTotal - descuento;
+				} else {
+					descuento = (precioTotal * 20) / 100;
+					precioTotalDescuento = precioTotal - descuento;
+				}
+			} else {
+				if (cantidadLamparas == 3) {
+					if (marca == "ArgentinaLuz") {
+						descuento = (precioTotal * 15) / 100;
+						precioTotalDescuento = precioTotal - descuento;
+					} else {
+						if (marca == "FelipeLamparas") {
+							descuento = (precioTotal * 10) / 100;
+							precioTotalDescuento = precioTotal - descuento;
+						} else {
+							descuento = (precioTotal * 5) / 100;
+							precioTotalDescuento = precioTotal - descuento;
+						}
+					}
+				} else {
+					precioTotalDescuento = precioTotal;
+				}
+			}
+		}
+	}
+
+	if (precioTotalDescuento > 120) {
+		ingresosBrutos = (precioTotalDescuento * 10) / 100;
+		precioTotalDescuento = precioTotalDescuento + ingresosBrutos;
+		alert("Usted pago " + ingresosBrutos + " de IIBB");
+	}
+
+	document.getElementById("txtIdprecioDescuento").value = precioTotalDescuento;
 }
