ce70dfa98866541984f35eb0541a2d0e4cf91ba3
Leanmura <murakoshi.lean@gmail.com>
2021-02-11 01:23:16 -0300

    prueba

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,57 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacionIngresada = document.getElementById('txtIdEstacion').value;
+	let destinoIngresado = document.getElementById('txtIdDestino').value;
+	let seViaja;
+	switch (estacionIngresada)
+	{
+		case 'Invierno':
+			switch (destinoIngresado)
+			{
+				case 'Bariloche':
+					seViaja = 1;
+					break;
+				default:
+					seViaja = 0;
+			}
+			break;
+
+		case 'Verano':
+			switch (destinoIngresado)
+			{
+				case 'Cataratas':
+				case 'Mar del plata':
+					seViaja = 1;
+					break;
+				default:
+					seViaja = 0;
+			}
+			break;
+
+		case 'Primavera':
+			switch (destinoIngresado)
+			{
+				case 'Bariloche':
+					seViaja = 0;
+					break;
+				default:
+					seViaja = 1;
+			}
+			break;
+
+		case 'Otoño':
+			seViaja = 1;
+			break;
+	}
+
+	if (seViaja == 1)
+	{
+		alert("Se viaja");
+	}
+	else
+	{
+		alert("No se viaja");
+	}
+
 
 }//FIN DE LA FUNCIÓN
