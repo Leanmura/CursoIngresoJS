5e385960764de6fadddeb71b526a4e05861032dd
Leanmura <murakoshi.lean@gmail.com>
2021-02-18 11:24:20 -0300

    Resuelto

--- 8-TPs/09-jsAgilidadAritmetica1.js
+++ 8-TPs/09-jsAgilidadAritmetica1.js
@@ -9,12 +9,53 @@ se debe informar si el resultado es el correcto o no.
 var respuesta;
 function comenzar()
 {
+    let numeroUno;
+    let numeroDos;
+    let operacion;
 
-	
+    numeroUno = Math.floor(Math.random() * 10) + 1;
+    numeroDos = Math.floor(Math.random() * 10) + 1;
+    operacion = Math.floor(Math.random() * 4) + 1;
+
+    switch (operacion)
+    {
+        case 1:
+            operacion = "+";
+            respuesta = numeroUno + numeroDos;
+            break;
+        case 2:
+            operacion = "-";
+            respuesta = numeroUno - numeroDos;
+            break;
+        case 3:
+            operacion = "x";
+            respuesta = numeroUno * numeroDos;
+            break;
+        case 4:
+            operacion = "/";
+            respuesta = numeroUno / numeroDos;
+            respuesta = respuesta.toFixed(2);
+
+    }
+
+    document.getElementById('txtIdPrimerNumero').value = numeroUno;
+    document.getElementById('txtIdOperador').value = operacion;
+    document.getElementById('txtIdSegundoNumero').value = numeroDos;
 
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+    let tuRespuesta;
+
+    tuRespuesta = document.getElementById('txtIdRespuesta').value;
+    tuRespuesta = parseFloat(tuRespuesta);
+    if (respuesta == tuRespuesta)
+    {
+        alert("Respuesta CORRECTA");
+    }
+    else
+    {
+        alert("Respuesta INCORRECTA");
+    }
 
 }//FIN DE LA FUNCIÓN
