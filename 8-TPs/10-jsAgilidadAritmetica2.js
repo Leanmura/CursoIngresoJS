2c1842124fc2217dd9f3ec7fdd6d61acc66bacf6
Leanmura <murakoshi.lean@gmail.com>
2021-02-18 11:42:03 -0300

    ndea

--- 8-TPs/10-jsAgilidadAritmetica2.js
+++ 8-TPs/10-jsAgilidadAritmetica2.js
@@ -7,11 +7,54 @@ var respuesta;
 var temporizador;
 function comenzar()
 {
-	
+    let numeroUno;
+    let numeroDos;
+    let operacion;
+
+    numeroUno = Math.floor(Math.random() * 10) + 1;
+    numeroDos = Math.floor(Math.random() * 10) + 1;
+    operacion = Math.floor(Math.random() * 4) + 1;
+
+    switch (operacion)
+    {
+        case 1:
+            operacion = "+";
+            respuesta = numeroUno + numeroDos;
+            break;
+        case 2:
+            operacion = "-";
+            respuesta = numeroUno - numeroDos;
+            break;
+        case 3:
+            operacion = "x";
+            respuesta = numeroUno * numeroDos;
+            break;
+        case 4:
+            operacion = "/";
+            respuesta = numeroUno / numeroDos;
+            respuesta = respuesta.toFixed(2);
+
+    }
+
+    document.getElementById('txtIdPrimerNumero').value = numeroUno;
+    document.getElementById('txtIdOperador').value = operacion;
+    document.getElementById('txtIDSegundoNumero').value = numeroDos;
+    temporizador = setTimeout(Responder, 4000);
+
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+    let tuRespuesta;
 
+    tuRespuesta = document.getElementById('txtIdRespuesta').value;
+    tuRespuesta = parseFloat(tuRespuesta);
+    if (respuesta == tuRespuesta)
+    {
+        alert("Respuesta CORRECTA");
+    }
+    else
+    {
+        alert("Respuesta INCORRECTA");
+    }
 
 }//FIN DE LA FUNCIÓN
