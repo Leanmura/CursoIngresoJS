19411e5c88f49a1af43c41b7dee919438dafc144
Leanmura <murakoshi.lean@gmail.com>
2021-02-26 12:59:01 -0300

    ejercicios adicionales

--- 6-InstruccionFor/jsIteracionesFor-06.js
+++ 6-InstruccionFor/jsIteracionesFor-06.js
@@ -1,7 +1,7 @@
 /* Leandro Murakoshi
 Enunciado:
 al presionar el botón pedir un número. mostrar los numeros pares desde el 1 al número ingresado, y mostrar la cantidad de numeros pares encontrados. */
-function mostrar()
+/* function mostrar()
 {
 	let contador;
 	let numeroIngresado;
@@ -24,3 +24,219 @@ function mostrar()
 
 
 }//FIN DE LA FUNCIÓN
+ */
+
+/* 
+Murakoshi Leandro
+Realizar el algoritmo que permita ingresar 5 paises:
+el continente (validar entre america , asia , europa ,africa y oceania) 
+el nombre del país, 
+cantidad de habitantes en millones entre 1 y 7000 (validar)
+el nivel de pobresa entre (pobre, rico , muy rico) en europa no hay paises pobre(validar)   
+la temperaruta mínima que se registra en su territorio(entre -50 y 50)  
+a)La cantidad de temperaturas pares.
+b)la cantidad de temperaturas impares de europa
+c)El nombre del pais con menos habitantes
+d)la cantidad de paises que superan los 40 grados.
+e)la cantidad de paises de america que tienen menos de 0 grados .
+f)el promedio de habitantes entre los paises ingresados .
+g)el promedio de habitantes entre los paises que superan los 40 grados        
+H) la temperatura mínima ingresada, y nombre del pais que registro esa temperatura. 
+i) que continente tiene mas habitantes. */
+
+
+function mostrar()
+{
+	let paisIngresado;
+	let contador;
+	let cantidadHabitantesIngresado;
+	let continenteIngresado;
+	let nivelPobrezaIngresado;
+	let temperaturaIngresada;
+	let cantidadTemperaturasPares;
+	let cantidadTemperaturasImparesEuropa;
+	let menorCantidadHabitantes;
+	let cantidadPaisesMasCuarentaGrados;
+	let cantidadPaisesAmericanosMenosZeroGrados;
+	let acumuladorHabitantes;
+	let promedioHabitantes;
+	let acumuladorHabitantesPaisesMasCuarentaGrados;
+	let temperaturaMinimaIngresada;
+	let paisTemperaturaMinima;
+	let cantidadHabitantesAmerica;
+	let cantidadHabitantesEuropa;
+	let cantidadHabitantesAfrica;
+	let cantidadHabitantesAsia;
+	let cantidadHabitantesOceania;
+	let paisMenorCantidadHabitantes;
+	let promedioHabitantesPaisesMasCuarentaGrados
+
+	cantidadHabitantesOceania = 0;
+	cantidadHabitantesAfrica = 0;
+	cantidadHabitantesAmerica = 0;
+	cantidadHabitantesAsia = 0;
+	cantidadHabitantesEuropa = 0;
+
+	acumuladorHabitantesPaisesMasCuarentaGrados = 0;
+	acumuladorHabitantes = 0;
+	cantidadPaisesAmericanosMenosZeroGrados = 0;
+	cantidadPaisesMasCuarentaGrados = 0;
+	cantidadTemperaturasImparesEuropa = 0;
+	cantidadTemperaturasPares = 0;
+
+	for (contador = 0; contador < 5; contador++)
+	{
+		paisIngresado = prompt("Ingresar pais #" + (contador + 1));
+		while (isNaN(paisIngresado) == false)
+		{
+			paisIngresado = prompt("Error. Ingresar pais #" + (contador + 1));
+		}
+
+		cantidadHabitantesIngresado = prompt("Ingresar cantidad de habitantes (millones) #" + (contador + 1));
+		cantidadHabitantesIngresado = parseInt(cantidadHabitantesIngresado);
+		while (isNaN(cantidadHabitantesIngresado) == true || cantidadHabitantesIngresado < 1 || cantidadHabitantesIngresado > 7000)
+		{
+			cantidadHabitantesIngresado = prompt("Error. Ingresar cantidad de habitantes (millones) #" + (contador + 1));
+		}
+
+		continenteIngresado = prompt("Ingresar continente #" + (contador + 1));
+		while (isNaN(continenteIngresado) == false || continenteIngresado != 'america' && continenteIngresado != 'europa' && continenteIngresado != 'oceania' && continenteIngresado != 'africa' && continenteIngresado != 'asia')
+		{
+			continenteIngresado = prompt("Error. Ingresar continente #" + (contador + 1));
+		}
+
+		nivelPobrezaIngresado = prompt("Ingresar el nivel de pobreza #" + (contador + 1));
+		while (isNaN(nivelPobrezaIngresado) == false || nivelPobrezaIngresado != 'muy rico' && nivelPobrezaIngresado != 'rico' && nivelPobrezaIngresado != 'pobre' || (continenteIngresado == 'europa' && nivelPobrezaIngresado == 'pobre'))
+		{
+			nivelPobrezaIngresado = prompt("Error. Ingresar el nivel de pobreza #" + (contador + 1));
+		}
+
+		temperaturaIngresada = prompt("Ingresar temperatura minima registrada #" + (contador + 1));
+		temperaturaIngresada = parseInt(temperaturaIngresada);
+		while (isNaN(temperaturaIngresada) == true || temperaturaIngresada < -50 || temperaturaIngresada > 50)
+		{
+			temperaturaIngresada = prompt("Error. Ingresar temperatura minima registrada #" + (contador + 1));
+		}
+
+		console.log(paisIngresado);
+		console.log(continenteIngresado);
+		console.log(cantidadHabitantesIngresado);
+		console.log(nivelPobrezaIngresado);
+		console.log(temperaturaIngresada);
+
+		if (temperaturaIngresada % 2 == 0)
+		{
+			cantidadTemperaturasPares++;
+		}
+		else
+		{
+			if (continenteIngresado == 'europa')
+			{
+				cantidadTemperaturasImparesEuropa++;
+			}
+		}
+
+		if (contador == 0)
+		{
+			menorCantidadHabitantes = cantidadHabitantesIngresado;
+			temperaturaMinimaIngresada = temperaturaIngresada;
+		}
+		else
+		{
+			if (cantidadHabitantesIngresado < menorCantidadHabitantes)
+			{
+				menorCantidadHabitantes = cantidadHabitantesIngresado;
+				paisMenorCantidadHabitantes = paisIngresado;
+			}
+			if (temperaturaIngresada < temperaturaMinimaIngresada)
+			{
+				temperaturaMinimaIngresada = temperaturaIngresada;
+				paisTemperaturaMinima = paisIngresado;
+			}
+		}
+
+		if (temperaturaIngresada > 40)
+		{
+			cantidadPaisesMasCuarentaGrados++;
+			acumuladorHabitantesPaisesMasCuarentaGrados = acumuladorHabitantesPaisesMasCuarentaGrados + cantidadHabitantesIngresado;
+		}
+
+		acumuladorHabitantes = acumuladorHabitantes + cantidadHabitantesIngresado;
+
+		switch (continenteIngresado)
+		{
+			case 'america':
+				cantidadHabitantesAmerica = cantidadHabitantesAmerica + cantidadHabitantesIngresado;
+				if (temperaturaIngresada < 0)
+				{
+					cantidadPaisesAmericanosMenosZeroGrados++;
+				}
+				break;
+			case 'oceania':
+				cantidadHabitantesOceania = cantidadHabitantesOceania + cantidadHabitantesIngresado;
+				break;
+			case 'africa':
+				cantidadHabitantesAfrica = cantidadHabitantesAfrica + cantidadHabitantesIngresado;
+				break;
+			case 'asia':
+				cantidadHabitantesAsia = cantidadHabitantesAsia + cantidadHabitantesIngresado;
+				break;
+			case 'europa':
+				cantidadHabitantesEuropa = cantidadHabitantesEuropa + cantidadHabitantesIngresado;
+				break;
+		}
+	}
+
+	if (cantidadHabitantesEuropa > cantidadHabitantesOceania && cantidadHabitantesEuropa > cantidadHabitantesAfrica && cantidadHabitantesEuropa > cantidadHabitantesAmerica && cantidadHabitantesEuropa > cantidadHabitantesAsia) 
+	{
+		alert("Europa es el pais con mayor poblacion");
+	}
+	else
+	{
+		if (cantidadHabitantesOceania > cantidadHabitantesAfrica && cantidadHabitantesOceania > cantidadHabitantesAmerica && cantidadHabitantesOceania > cantidadHabitantesAsia)
+		{
+			alert("Oceania es el pais con mayor poblacion");
+		}
+		else
+		{
+			if (cantidadHabitantesAfrica > cantidadHabitantesAmerica && cantidadHabitantesAfrica > cantidadHabitantesAsia)
+			{
+				alert("Africa es el pais con mayor poblacion");
+			}
+			else
+			{
+				if (cantidadHabitantesAmerica > cantidadHabitantesAsia)
+				{
+					alert("America es el pais con mayor poblacion");
+				}
+				else
+				{
+					alert("Asia es el pais con mayor poblacion");
+				}
+			}
+		}
+	}
+	promedioHabitantes = acumuladorHabitantes / 5;
+	promedioHabitantesPaisesMasCuarentaGrados = acumuladorHabitantesPaisesMasCuarentaGrados / cantidadPaisesMasCuarentaGrados;
+
+	console.log("Cantidad de temperaturas pares: " + cantidadTemperaturasPares);
+	console.log("Cantidad de temperaturas impares de Europa: " + cantidadTemperaturasImparesEuropa);
+	console.log("Nombre del pais con menos habitantes: " + paisMenorCantidadHabitantes);
+	console.log("Cantidad de paises que superan los cuarenta grados: " + cantidadPaisesMasCuarentaGrados);
+	console.log("Cantidad de paises de America con menos de 0 grados: " + cantidadPaisesAmericanosMenosZeroGrados);
+	console.log("Promedio de habitantes: " + promedioHabitantes);
+
+	if (cantidadPaisesMasCuarentaGrados == 0)
+	{
+		console.log("No hay paises con mas de cuarenta grados.");
+
+	}
+	else
+	{
+		console.log("Promedio de habitantes de los paises que superan los 40grados: " + promedioHabitantesPaisesMasCuarentaGrados);
+	}
+
+	console.log("Temperatura minima ingresada: " + temperaturaMinimaIngresada);
+	console.log("Esta corresponde a " + paisTemperaturaMinima);
+
+}
--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -82,7 +82,7 @@ For 11 (a 5 personas nombre , sexo ,edad(validar que solo sean hombres no
 	mayores a 12 y que las mujeres sean ADOLESCENTES),altura(rangos que quiera)) 
 	... informar el sexo y nombre de la persona mas alta */
 
-function mostrar()
+/* function mostrar()
 {
 	let contador;
 	let nombre;
@@ -144,4 +144,289 @@ function mostrar()
 	}
 	alert("Nombre del mas alto: " + nombreDelMasAlto);
 	alert("Sexo de " + nombreDelMasAlto + ": " + sexoDelMasAlto);
+}
+ */
+
+/* Idem, 11 pero de las mujeres el nombre de la mas joven, 
+de los hombres el nombre del mas bajito, solo si los hay */
+/* function mostrar()
+{
+	let contador;
+	let nombre;
+	let sexo;
+	let edad;
+	let altura;
+
+	let mayorAltura;
+	let sexoDelMasAlto;
+	let nombreDelMasAlto;
+
+	let menorEdadMujer;
+	let nombreDeLaMasJoven;
+
+	let menorAlturaHombre;
+	let nombreDelHombreMasBajo;
+
+	let banderaPrimerHombreMasBajo;
+	let banderaPrimeroAltura
+	let banderaPrimeroEdadMujer;
+
+	banderaPrimeroAltura = 'primero';
+	banderaPrimeroEdadMujer = 'primero';
+	banderaPrimerHombreMasBajo = 'primero';
+
+	for (contador = 0; contador < 5; contador++)
+	{
+		nombre = prompt("Ingrese nombre #" + (contador + 1));
+		while (isNaN(nombre) == false)
+		{
+			nombre = prompt("Error. Ingrese nombre #" + (contador + 1));
+		}
+
+		sexo = prompt("Ingrese sexo #" + (contador + 1));
+		while (isNaN(sexo) == false || sexo != 'masculino' && sexo != 'femenino')
+		{
+			sexo = prompt("Error. Ingrese sexo #" + (contador + 1));
+		}
+
+		edad = prompt("Ingrese edad #" + (contador + 1));
+		edad = parseInt(edad);
+		while (isNaN(edad) == true || (sexo == 'masculino' && edad > 12) || (sexo == 'femenino' && (edad > 17 || edad < 13)))
+		{
+			edad = prompt("Error. Ingrese edad #" + (contador + 1));
+			edad = parseInt(edad);
+
+		}
+
+		altura = prompt("Ingrese altura #" + (contador + 1));
+		while (isNaN(altura) == true || altura < 1.30 || altura > 2.20)
+		{
+			altura = prompt("Error. Ingrese altura #" + (contador + 1));
+			altura = parseFloat(altura);
+
+		}
+		altura = parseFloat(altura);
+
+		console.log(nombre);
+		console.log(sexo);
+		console.log(edad);
+		console.log(altura);
+
+		if (banderaPrimeroAltura == 'primero' || altura > mayorAltura)
+		{
+			mayorAltura = altura;
+			banderaPrimeroAltura = 'no es el primero';
+			sexoDelMasAlto = sexo;
+			nombreDelMasAlto = nombre;
+		}
+
+		if (sexo == 'masculino')
+		{
+			if (banderaPrimerHombreMasBajo == 'primero' || altura < menorAlturaHombre)
+			{
+				menorAlturaHombre = altura;
+				banderaPrimerHombreMasBajo = 'no es el primero';
+				nombreDelHombreMasBajo = nombre;
+			}
+		}
+		else
+		{
+			if (banderaPrimeroEdadMujer == 'primero' || edad < menorEdadMujer)
+			{
+				menorEdadMujer = edad;
+				banderaPrimeroEdadMujer = 'no es el primero';
+				nombreDeLaMasJoven = nombre;
+			}
+
+		}
+
+
+
+	}
+	alert("Nombre del mas alto: " + nombreDelMasAlto);
+	alert("Sexo de " + nombreDelMasAlto + ": " + sexoDelMasAlto);
+
+	if (banderaPrimerHombreMasBajo == 'primero')
+	{
+		alert("No hay Hombres");
+	}
+	else
+	{
+		alert("El nombre del hombre mas bajo es: " + nombreDelHombreMasBajo);
+	}
+
+	if (banderaPrimeroEdadMujer == 'primero')
+	{
+		alert("No hay mujeres");
+	}
+	else
+	{
+		alert("El nombre de la mujer mas joven es: " + nombreDeLaMasJoven)
+	}
+}
+ */
+/* 
+Murakoshi Leandro
+For 13 ( o anterior mas ...el promedio de edad entre los hombre ,el 
+promedio de edad entre las mujeres, la cantidad de personas que miden mas 
+de 1,60 metros, y el porcentaje de mujeres sobre el total de personas de mas de 1,60 mts */
+
+function mostrar()
+{
+	let contador;
+	let nombre;
+	let sexo;
+	let edad;
+	let altura;
+
+	let mayorAltura;
+	let sexoDelMasAlto;
+	let nombreDelMasAlto;
+
+	let menorEdadMujer;
+	let nombreDeLaMasJoven;
+
+	let menorAlturaHombre;
+	let nombreDelHombreMasBajo;
+
+	let banderaPrimerHombreMasBajo;
+	let banderaPrimeroAltura
+	let banderaPrimeroEdadMujer;
+
+	let promedioEdadHombres;
+	let promedioEdadMujeres;
+	let cantidadMujeres;
+	let cantidadHombres;
+	let acumuladorEdadMujeres;
+	let acumuladorEdadHombres;
+	let cantidadPersonasMasDeUnoSesenta;
+	let cantidadMujeresMasDeUnoSesenta;
+	let porcentajeMujeresMasDeUnoSesenta
+
+	banderaPrimeroAltura = 'primero';
+	banderaPrimeroEdadMujer = 'primero';
+	banderaPrimerHombreMasBajo = 'primero';
+
+	acumuladorEdadHombres = 0;
+	acumuladorEdadMujeres = 0;
+	cantidadHombres = 0;
+	cantidadMujeres = 0;
+	cantidadPersonasMasDeUnoSesenta = 0;
+	cantidadMujeresMasDeUnoSesenta = 0;
+
+	for (contador = 0; contador < 3; contador++)
+	{
+		nombre = prompt("Ingrese nombre #" + (contador + 1));
+		while (isNaN(nombre) == false)
+		{
+			nombre = prompt("Error. Ingrese nombre #" + (contador + 1));
+		}
+
+		sexo = prompt("Ingrese sexo #" + (contador + 1));
+		while (isNaN(sexo) == false || sexo != 'masculino' && sexo != 'femenino')
+		{
+			sexo = prompt("Error. Ingrese sexo #" + (contador + 1));
+		}
+
+		edad = prompt("Ingrese edad #" + (contador + 1));
+		edad = parseInt(edad);
+		while (isNaN(edad) == true || (sexo == 'masculino' && edad > 12) || (sexo == 'femenino' && (edad > 17 || edad < 13)))
+		{
+			edad = prompt("Error. Ingrese edad #" + (contador + 1));
+			edad = parseInt(edad);
+
+		}
+
+		altura = prompt("Ingrese altura #" + (contador + 1));
+		while (isNaN(altura) == true || altura < 1.30 || altura > 2.20)
+		{
+			altura = prompt("Error. Ingrese altura #" + (contador + 1));
+			altura = parseFloat(altura);
+
+		}
+		altura = parseFloat(altura);
+
+		console.log(nombre);
+		console.log(sexo);
+		console.log(edad);
+		console.log(altura);
+
+		if (banderaPrimeroAltura == 'primero' || altura > mayorAltura)
+		{
+			mayorAltura = altura;
+			banderaPrimeroAltura = 'no es el primero';
+			sexoDelMasAlto = sexo;
+			nombreDelMasAlto = nombre;
+		}
+
+		if (sexo == 'masculino')
+		{
+			if (banderaPrimerHombreMasBajo == 'primero' || altura < menorAlturaHombre)
+			{
+				menorAlturaHombre = altura;
+				banderaPrimerHombreMasBajo = 'no es el primero';
+				nombreDelHombreMasBajo = nombre;
+			}
+			acumuladorEdadHombres = acumuladorEdadHombres + edad;
+			cantidadHombres++;
+		}
+		else
+		{
+			if (banderaPrimeroEdadMujer == 'primero' || edad < menorEdadMujer)
+			{
+				menorEdadMujer = edad;
+				banderaPrimeroEdadMujer = 'no es el primero';
+				nombreDeLaMasJoven = nombre;
+			}
+			acumuladorEdadMujeres = acumuladorEdadMujeres + edad;
+			cantidadMujeres++;
+		}
+
+		if (altura > 1.6)
+		{
+			cantidadPersonasMasDeUnoSesenta++;
+			if (sexo == 'femenino')
+			{
+				cantidadMujeresMasDeUnoSesenta++;
+			}
+		}
+
+
+
+	}
+
+	promedioEdadHombres = acumuladorEdadHombres / cantidadHombres;
+	promedioEdadMujeres = acumuladorEdadMujeres / cantidadMujeres;
+	porcentajeMujeresMasDeUnoSesenta = cantidadMujeresMasDeUnoSesenta * 100 / cantidadPersonasMasDeUnoSesenta;
+
+	alert("Nombre del mas alto: " + nombreDelMasAlto);
+	alert("Sexo de " + nombreDelMasAlto + ": " + sexoDelMasAlto);
+
+	if (banderaPrimerHombreMasBajo == 'primero')
+	{
+		alert("No hay Hombres");
+	}
+	else
+	{
+		alert("El nombre del hombre mas bajo es: " + nombreDelHombreMasBajo);
+		alert("Promedio de las edades de los hombres: " + promedioEdadHombres);
+	}
+
+	if (banderaPrimeroEdadMujer == 'primero')
+	{
+		alert("No hay mujeres");
+	}
+	else
+	{
+		alert("El nombre de la mujer mas joven es: " + nombreDeLaMasJoven);
+		alert("Promedio de las edades de las mujeres: " + promedioEdadMujeres);
+		if (cantidadPersonasMasDeUnoSesenta == 0)
+		{
+			alert("No hay personas de mas de 1.60m.")
+		}
+		else
+		{
+			alert("El procentaje de las mujeres que miden mas de 1.60m es: " + porcentajeMujeresMasDeUnoSesenta);
+		}
+	}
 }
